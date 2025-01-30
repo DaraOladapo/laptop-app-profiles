aed7ac1adbbea6f414a924807aa1d38dbc44f156
Dara Oladapo <daraoladapo@outlook.com>
2025-01-30 22:39:48 +0000

    Add new WPF app and core projects
    Updated `LaptopAppProfiles.sln` to include `LaptopAppProfiles.App` and `LaptopAppProfiles.Core` projects with configuration platforms. Added `App.xaml` and `App.xaml.cs` for application setup, and `AssemblyInfo.cs` for theme information. Created `LaptopAppProfiles.App.csproj` for WPF app targeting .NET 9.0. Added `MainWindow.xaml` and `MainWindow.xaml.cs` for main window definition. Created `LaptopAppProfiles.Core.csproj` for core project targeting .NET 9.0. Added `AppProfiles.cs` and `Category.cs` in `LaptopAppProfiles.Core.Entities` namespace to define `App` and `Category` classes.

---
+++ src/LaptopAppProfiles.Core/Entities/AppProfiles.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace LaptopAppProfiles.Core.Entities;
+
+public class App
+{
+    public Guid ProfileID { get; set; }
+    public required string Name { get; set; }
+    public virtual required ICollection<Category> Categories { get; set; }
+}
---
+++ src/LaptopAppProfiles.Core/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace LaptopAppProfiles.Core.Entities;
+
+public class Category
+{
+    public Guid CategoryID { get; set; }
+    public required string Name { get; set; }
+    public virtual required ICollection<App> Apps { get; set; }
+}
